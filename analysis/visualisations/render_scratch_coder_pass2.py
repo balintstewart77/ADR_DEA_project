628e1f0bd8f2b67b5b11988d6844138269232ef7
Balint Stewart <balintstewart@gmail.com>
2026-09-16 13:33:05 +0100

    Number the overlapping Figure 3 policy label

--- analysis/visualisations/render_scratch_coder_pass2.py
+++ analysis/visualisations/render_scratch_coder_pass2.py
@@ -270,7 +270,7 @@ def render_s1(rows: list[dict], base: Path) -> tuple[dict, dict]:
 
 DIRECT_LABELS = {
     "Research Domains": {1, 4, 8},
-    "Analytical Purposes": {1, 4, 5},
+    "Analytical Purposes": {1, 4},
 }
 
 
